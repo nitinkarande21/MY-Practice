d54bec588ff339812528e91d1078992ba1d8c0a9
nitinkarande21 <nitinkarande93@gmail.com>
2023-10-23 10:41:25 +0530

    all files added

---
+++ variables.js
@@ -0,0 +1,34 @@
+//Variable:are the container to store DataTransfer(data type)
+//let/var/const deafult var (keywards)followed by variable name 
+//Space not allowed 
+//first letter should always alaphabet,spacial charcters not allowed 
+//always write as camel case
+//spacial charecter not allowed(exepct $)
+//let myFirstName = 'Nitin'; declartion and defination 
+
+//let myFirstName = 'Nitin';
+myF= 'dilip'
+var mySName = 'Karande';
+
+
+var myName;
+
+myName = 'Nitin' ;
+
+myName = 'Aarya' ;
+
+const myCity = 'pune'; //const keyword intialization value is must.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
