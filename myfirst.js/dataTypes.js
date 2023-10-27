ae3cf510d7d9cd8ae9abe14b68b9ba0946e65ae1
nitinkarande21 <nitinkarande93@gmail.com>
2023-10-27 11:57:04 +0530

    dataTypes add

--- myfirst.js/dataTypes.js
+++ myfirst.js/dataTypes.js
@@ -5,14 +5,41 @@
 //String, bigint, Boolean, number, undefined, null, symbol,object
 //Object data type can be an Array,an object, a date
 
-//string
+//string Datatypes
 
-let state="";       //empty string
+let state = "";       //empty string
 
-let city ="pune";   //string data type
+let city = "pune";   //string data type
 
 console.log(city);
 
 city = "Mumbai";
 
 console.log(city);
+
+//Number Datatypes
+
+var marks = 90;   //number
+console.log('marks', marks);
+
+var mobNo = 7972637703;
+console.log(mobNo);
+
+//boolean datatypes
+
+var isfavfruit = false;
+console.log(isfavfruit);
+
+//typeof() it gives type of variable
+console.log('data type of isfavfruit var', typeof (isfavfruit));
+
+const match = undefined;
+console.log(match);
+
+let abc;
+console.log(abc);
+console.log(typeof (abc));
+
+var myName = null;
+console.log(myName);
+console.log(typeof (myName));
