46a6752ac6de52c2b13affbda133ee380ba7eeb0
nitinkarande21 <nitinkarande93@gmail.com>
2024-01-30 23:07:32 +0530

    all changes added

---
+++ REACTJS/todolist/src/App.js
@@ -0,0 +1,18 @@
+// how to create component 
+// functional componant
+// first letter should be capital of component 
+
+const App=()=> {
+  return (
+    <div>
+      <h1>Hello this is my first componant</h1>
+      <img src="https://in.images.search.yahoo.com/search/images;_ylt=AwrPphzZLrllz5gx2gu9HAx.;_ylu=c2VjA3NlYXJjaARzbGsDYnV0dG9u;_ylc=X1MDMjExNDcyMzAwNQRfcgMyBGZyA21jYWZlZQRmcjIDcDpzLHY6aSxtOnNiLXRvcARncHJpZANCeTdSMkdIYVFoV05EZmI2SzhIOEtBBG5fcnNsdAMwBG5fc3VnZwMxMARvcmlnaW4DaW4uaW1hZ2VzLnNlYXJjaC55YWhvby5jb20EcG9zAzAEcHFzdHIDBHBxc3RybAMwBHFzdHJsAzYEcXVlcnkDZmxvd2VyBHRfc3RtcAMxNzA2NjM1ODgz?p=flower&fr=mcafee&fr2=p%3As%2Cv%3Ai%2Cm%3Asb-top&ei=UTF-8&x=wrt&type=E210IN885G0#id=8&iurl=https%3A%2F%2Fwww.publicdomainpictures.net%2Fpictures%2F30000%2Fvelka%2Fnice-flower-1337198953S0C.jpg&action=click"/>
+      </div>
+
+      )
+
+
+
+
+}
+      export default App;
